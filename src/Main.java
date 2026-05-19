ccee1706b957bcf2ca582e9feb8b55caa6559bb3
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-18 22:16:50 -0400

    Metodo para registrar y mostrar los pacientes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Paciente> listaPacientes = new ArrayList<>();
+
     // Metodo para mostrar menu
     public static void mostrarMenu() {
         System.out.println("\n---------------------------------------");
@@ -11,5 +17,46 @@ public class Main {
         System.out.println("  5. Mostrar pacientes registrados");
         System.out.println("  6. Salir del sistema");
         System.out.println("---------------------------------------");
+    }
+
+    // Metodo para registrar los pacientes
+    public static void registrarPacientes() {
+        System.out.println("\nREGISTRO DE PACIENTE\n");
+
+        System.out.print("Ingrese el nombre del paciente: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese la edad del paciente: ");
+        int edad = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Ingrese el sexo del paciente: ");
+        String sexo = scanner.nextLine();
+
+        System.out.print("Ingrese la altura del paciente: ");
+        float altura = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.print("Ingrese el peso del paciente: ");
+        float peso = scanner.nextFloat();
+        scanner.nextLine();
+
+        Paciente paciente = new Paciente(nombre, edad, sexo, altura, peso);
+
+        listaPacientes.add(paciente);
+    }
+
+    // Metodo para mostrar pacientes registrados
+    public static void mostrarPacientesRegistrados() {
+        if (listaPacientes.isEmpty()) {
+            System.out.println("\nNo hay pacientes registrados");
+        }
+        else {
+            System.out.println("\n-------Registro de Pacientes-------");
+            for (Paciente p : listaPacientes) {
+                Paciente.mostrarDatosPaciente(p);
+            }
+            System.out.println("-----------------------------------");
+        }
     }
 }
