d2e4c94982c8805ffd0376546ee0bae5712c9220
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-18 20:26:52 -0400

    Creacion de atributos, constructores, getters y setter de la clase Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,3 +1,55 @@
 public class Paciente {
-    
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private float altura;
+    private float peso;
+
+    public Paciente() {
+        setNombre("");
+        setEdad(0);
+        setSexo("");
+        setAltura(0);
+        setPeso(0);
+    }
+
+    public Paciente(String nombre, int edad, String sexo, float altura, float peso) {
+        setNombre(nombre);
+        setEdad(edad);
+        setSexo(sexo);
+        setAltura(altura);
+        setPeso(peso);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
 }
