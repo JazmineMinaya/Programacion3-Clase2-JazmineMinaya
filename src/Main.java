56f3e800d3322466e6bf4a927acbbddcb73c4186
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-18 20:07:03 -0400

    Metodo crear menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
-    
+    // Metodo pra mostrar menu
+    public static void mostrarMenu() {
+        System.out.println("\n---------------------------------------");
+        System.out.println("                 MENU");
+        System.out.println("---------------------------------------");
+        System.out.println("  1. Registrar paciente");
+        System.out.println("  2. Mostrar informacion de pacientes");
+        System.out.println("  3. Calcular IMC");
+        System.out.println("  4. Determinar peso del paciente");
+        System.out.println("  5. Mostrar pacientes registrados");
+        System.out.println("  6. Salir del sistema");
+        System.out.println("---------------------------------------");
+    }
 }
